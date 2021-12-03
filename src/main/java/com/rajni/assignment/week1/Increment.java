7c9ef0cf15fb513d5c1f5133bb072212dacf9609
rajniubhi <ubhi.rajni800@gmail.com>
2021-12-03 18:52:04 +0530

    Incremnet problem issue fixed for test cases and runtime limit exceeded

--- src/main/java/com/rajni/assignment/week1/Increment.java
+++ src/main/java/com/rajni/assignment/week1/Increment.java
@@ -1,18 +1,28 @@
 package com.rajni.assignment.week1;
 
-import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Increment {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        BigInteger integer = new BigInteger(scanner.next()).add(new BigInteger("1"));
-        int digits = 0;
-        BigInteger divisor = new BigInteger("10");
-        while (integer.compareTo(BigInteger.ZERO) != 0) {
-            digits++;
-            integer = integer.divide(divisor);
+
+        String str = scanner.next();
+        if(str.endsWith("9") && (str.startsWith("9") || str.length() == 1)) {
+            boolean addOne = true;
+            for (int i = 0 ; i <  str.length(); i++) {
+                if(str.charAt(i) != '9') {
+                    addOne = false;
+                    break;
+                }
+            }
+            if(addOne) {
+                System.out.println(str.length() + 1);
+            } else {
+                System.out.println(str.length());
+            }
+        } else {
+            System.out.println(str.length());
         }
-        System.out.println(digits);
+
     }
 }
